f3e09e493d1b8bcfb8adae47bf464da3b49e4ed6
Maxim <128475914+lequder@users.noreply.github.com>
2023-11-19 12:48:09 +0700

    Update task_2.c

--- homework_2/task_2.c
+++ homework_2/task_2.c
@@ -6,15 +6,26 @@ int main()
 	printf("Enter N: ");
 	scanf("%d", &N);
 	int array[N];
+	int temp;
+	int start = 0;
+	int end = N - 1;
 	
 	for (int i = 0; i < N; i++){
 		printf("Enter array[%d] = ", i);
 		scanf("%d", &array[i]);
 	}
 	
+	while (start < end) {
+		temp = array[start];
+		array[start] = array[end];
+		array[end] = temp;
+		start++;
+		end--;
+	}
+	
 	printf("\nReverse array: ");
 	
-	for (int i = N-1; i >= 0; i--){
+	for (int i = 0; i < N; i++){
 		printf("%d ", array[i]);
 	}
 
